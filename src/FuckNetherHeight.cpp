cc4f26d2a3a5fa8fbfafa152da2a00155eeed24a
dofes <91889957+dofes@users.noreply.github.com>
2024-01-29 08:52:51 +0800

    fix: add dimension_utils namespace and implement functions for sending empty chunks and faking dimension change

--- src/FuckNetherHeight.cpp
+++ src/FuckNetherHeight.cpp
@@ -1,6 +1,60 @@
 #include "Global.h"
 #include "TCHelper.h"
 
+namespace dimension_utils {
+void sendEmptyChunk(const NetworkIdentifier& netId, int chunkX, int chunkZ, bool forceUpdate) {
+    std::array<uchar, 4096> biome{};
+    LevelChunkPacket        levelChunkPacket;
+    BinaryStream            binaryStream{levelChunkPacket.mSerializedChunk, false};
+    VarIntDataOutput        varIntDataOutput(&binaryStream);
+
+    varIntDataOutput.writeBytes(&biome, 4096); // write void biome
+    for (int i = 1; i < 8; i++) {
+        varIntDataOutput.writeByte((127 << 1) | 1);
+    }
+    varIntDataOutput.mStream->writeUnsignedChar(0); // write border blocks
+
+    levelChunkPacket.mPos.x          = chunkX;
+    levelChunkPacket.mPos.z          = chunkZ;
+    levelChunkPacket.mCacheEnabled   = false;
+    levelChunkPacket.mSubChunksCount = 0;
+
+    ll::service::getLevel()->getPacketSender()->sendToClient(netId, levelChunkPacket, SubClientId::PrimaryClient);
+
+    if (forceUpdate) {
+        NetworkBlockPosition pos{chunkX << 4, 80, chunkZ << 4};
+        UpdateBlockPacket    blockPacket;
+        blockPacket.mPos         = pos;
+        blockPacket.mLayer       = UpdateBlockPacket::BlockLayer::Standard;
+        blockPacket.mUpdateFlags = BlockUpdateFlag::Neighbors;
+        ll::service::getLevel()->getPacketSender()->sendToClient(netId, blockPacket, SubClientId::PrimaryClient);
+    }
+}
+
+void sendEmptyChunks(const NetworkIdentifier& netId, const Vec3& position, int radius, bool forceUpdate) {
+    int chunkX = (int)(position.x) >> 4;
+    int chunkZ = (int)(position.z) >> 4;
+    for (int x = -radius; x <= radius; x++) {
+        for (int z = -radius; z <= radius; z++) {
+            sendEmptyChunk(netId, chunkX + x, chunkZ + z, forceUpdate);
+        }
+    }
+}
+
+void fakeChangeDimension(
+    const NetworkIdentifier& netId,
+    ActorRuntimeID           runtimeId,
+    DimensionType            fakeDimId,
+    const Vec3&              pos
+) {
+    ChangeDimensionPacket changeDimensionPacket{fakeDimId, pos, true};
+    ll::service::getLevel()->getPacketSender()->sendToClient(netId, changeDimensionPacket, SubClientId::PrimaryClient);
+    PlayerActionPacket playerActionPacket{PlayerActionType::ChangeDimensionAck, runtimeId};
+    ll::service::getLevel()->getPacketSender()->sendToClient(netId, playerActionPacket, SubClientId::PrimaryClient);
+    sendEmptyChunks(netId, pos, 3, true);
+}
+} // namespace dimension_utils
+
 void PatchNetherHeight() {
     uintptr_t regionStart =
         (uintptr_t)ll::memory::resolveSymbol("??0NetherDimension@@QEAA@AEAVILevel@@AEAVScheduler@@@Z");
@@ -49,6 +103,29 @@ LL_AUTO_TYPE_INSTANCE_HOOK(
         this->mDimensionId = 2;
     }
     return origin(bs);
+}
+
+
+LL_TYPE_INSTANCE_HOOK(
+    LevelrequestPlayerChangeDimensionHandler,
+    HookPriority::Normal,
+    Level,
+    "?requestPlayerChangeDimension@Level@@UEAAXAEAVPlayer@@V?$unique_ptr@VChangeDimensionRequest@@U?$default_delete@"
+    "VChangeDimensionRequest@@@std@@@std@@@Z",
+    void,
+    Player&                                 player,
+    std::unique_ptr<ChangeDimensionRequest> changeRequest
+) {
+    auto inId = player.getDimensionId();
+    if ((inId == 1 && changeRequest->mToDimensionId == 2) || (inId == 2 && changeRequest->mToDimensionId == 1)) {
+        dimension_utils::fakeChangeDimension(
+            player.getNetworkIdentifier(),
+            player.getRuntimeID(),
+            0,
+            player.getPosition()
+        );
+    }
+    return origin(player, std::move(changeRequest));
 }
 
 LL_AUTO_TYPE_INSTANCE_HOOK(
