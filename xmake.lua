4c3b549f3003b46b0616ea425fe43e442dac4b1b
zimuya4153 <limulu10@163.com>
2025-10-19 20:26:18 +0800

    feat: adapt levilamina 1.4.x

--- xmake.lua
+++ xmake.lua
@@ -6,10 +6,9 @@ add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
 -- add_requires("levilamina x.x.x") for a specific version
 -- add_requires("levilamina develop") to use develop version
 -- please note that you should add bdslibrary yourself if using dev version
-add_requires("levilamina 1.3.2", {configs = {target_type = "server"}})
-add_requires("gmlib 1.3.0")
-
-add_requires("levibuildscript")
+add_requires("levilamina 1.4.4", {configs = {target_type = "server"}})
+add_requires("gmlib 1.4.3")
+add_requires("levibuildscript 0.5.2")
 
 if not has_config("vs_runtime") then
     set_runtimes("MD")
