286bbb9ced87d8a097c09b428a1535bbfea64aee
killcerr <kill_cerr@outlook.com>
2025-11-08 13:26:14 +0800

    feat: use pch to speed up build

--- xmake.lua
+++ xmake.lua
@@ -11,6 +11,7 @@ if not has_config("vs_runtime") then
 end
 
 target("FuckNetherHeight")
+    set_pcxxheader("src/pch.h")
     add_rules("@levibuildscript/linkrule")
     add_rules("@levibuildscript/modpacker")
     add_cxflags( "/EHa", "/utf-8", "/W4", "/w44265", "/w44289", "/w44296", "/w45263", "/w44738", "/w45204")
